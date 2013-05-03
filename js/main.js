26f00deb344360ce9bcc96eb80c9ee4741a9adf3
Eric <Ericrius1@gmail.com>
2013-05-03 15:02:46 -0700

    Box Spins

--- js/main.js
+++ js/main.js
@@ -1,43 +1,66 @@
-require(['goo'], function() {
+require({
+    // configure our AMD loader
+    //  baseUrl: './lib',
+    paths: {
+        // package mappings
+        goo: 'https://bitbucket.org/gootech/tutorials/downloads/goo'
+    }
+}, ['goo'], function () {
     require([
         'goo/entities/GooRunner',
-        'goo/renderer/Camera', //Include the module
+        'goo/entities/EntityUtils',
+        'goo/renderer/Material',
+        'goo/renderer/Camera',
         'goo/entities/components/CameraComponent',
         'goo/shapes/ShapeCreator',
-        'goo/entities/EntityUtils',
+        'goo/entities/components/ScriptComponent',
         'goo/renderer/shaders/ShaderLib',
-        'goo/renderer/Material'
-    ], function(
-        GooRunner,
-        Camera,  //Place module in this variable
-        CameraComponent,
-        ShapeCreator,
-        EntityUtils ,
-        ShaderLib,
-        Material
-    ) {
-        //Create new instance of Goo Engine
-        var goo = new GooRunner();
+        'goo/entities/World'], function (
+    GooRunner,
+    EntityUtils,
+    Material,
+    Camera,
+    CameraComponent,
+    ShapeCreator,
+    ScriptComponent,
+    ShaderLib,
+    World) {
+        // "use strict"; Helps you make fewer errors by detecting more things that could lead to breakages.
+        "use strict";
 
-        //Atach the Goo Engine to the DOM
-        //goo.renderer.domElement is a <canvas> element
+        // Initialize
+        var goo = new GooRunner();
         document.body.appendChild(goo.renderer.domElement);
 
-        var cameraEntity = goo.world.createEntity('Camera');
-        var camera = new Camera(35, 1, 0.1, 1000);
-        cameraEntity.setComponent(new CameraComponent(camera));
-
-        //position camera in the world
-        cameraEntity.transformComponent.transform.translation.set(0, 0, 5);
-        cameraEntity.addToWorld();
-
-        //Create box
+            
+        // Create box
         var meshData = ShapeCreator.createBox(1, 1, 1);
         var boxEntity = EntityUtils.createTypicalEntity(goo.world, meshData);
         var material = Material.createMaterial(ShaderLib.texturedLit, 'BoxMaterial');
+            
         boxEntity.meshRendererComponent.materials.push(material);
 
-        //Make boxx visible- adds entity to workd
-        boxEntity.addToWorld(); 
+            
+        // Spin the box
+        boxEntity.setComponent(new ScriptComponent({
+            run: function (entity) {
+                entity.transformComponent.transform.setRotationXYZ(
+                World.time * 1.2,
+                World.time * 2.0,
+                0);
+                entity.transformComponent.setUpdated();
+            }
+        }));
+
+        // Make box visible
+        boxEntity.addToWorld();
+
+            
+        // Add camera
+        var camera = new Camera(35, 1, 0.1, 1000);
+        var cameraEntity = goo.world.createEntity('Camera');
+        cameraEntity.setComponent(new CameraComponent(camera));
+        cameraEntity.transformComponent.transform.translation.set(0, 0, 5);
+        cameraEntity.addToWorld();
     });
 });
